24180280e006768cef19f830d8e5d0ba6c46d038
cijibin314 <coltonflather@gmail.com>
2025-12-31 18:19:48 -0500

    render 6

--- build.sh
+++ build.sh
@@ -4,5 +4,6 @@ set -o errexit
 
 pip install -r requirements.txt
 
-python leakfix_mvp/manage.py collectstatic --noinput
-python leakfix_mvp/manage.py migrate
+cd leakfix_mvp
+python manage.py collectstatic --noinput
+python manage.py migrate
